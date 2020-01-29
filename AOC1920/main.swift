a7ad73764fb06bd52d4eb9e00f5fe1d646361ff1
Heiko Goes <heiko.goes@gaebler-goes.de>
2020-01-29 17:52:06 +0100

    Initial Commit

---
+++ AOC1920/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AOC1920
+//
+//  Created by Heiko Goes on 29.01.20.
+//  Copyright © 2020 Heiko Goes. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
